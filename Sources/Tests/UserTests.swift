ed0b7405b759f3061cc637336239775e9ac0dafd
Adrian Kosmaczewski <learn@akosma.training>
2017-10-13 21:14:45 +0200

    Made the test generate much more notes

--- Sources/Tests/UserTests.swift
+++ Sources/Tests/UserTests.swift
@@ -5,10 +5,7 @@ final public class UserTests: APITest {
     var email = ""
     var name = ""
     var password = ""
-    let note = [
-        "title": "test title",
-        "contents" : "test note created from swift"
-    ]
+    var notesCount = 0
 
     override func scenario() -> [(String, APITest.TestMethod)]? {
         return [
@@ -16,7 +13,11 @@ final public class UserTests: APITest {
             ("login", login),
             ("noNotes", noNotes),
             ("createNote", createNote),
-            ("oneNote", oneNote),
+            ("createNote", createNote),
+            ("createNote", createNote),
+            ("createNote", createNote),
+            ("createNote", createNote),
+            ("countNotes", countNotes),
             ("logout", logout),
         ]
     }
@@ -64,20 +65,22 @@ final public class UserTests: APITest {
     }
 
     func createNote() throws {
+        let note = makeNote()
         engine.auth = .token(token)
         let response = try engine.post("/api/v1/notes", data: note)
         try expectEquals(200, response.status)
         try expectEquals("application/json; charset=utf-8", response.contentType)
+        notesCount += 1
     }
 
-    func oneNote() throws {
+    func countNotes() throws {
         engine.auth = .token(token)
         let response = try engine.get("/api/v1/notes")
         try expectEquals(200, response.status)
         try expectEquals("application/json; charset=utf-8", response.contentType)
         if let json = response.json {
             let notes = json["response"] as! [Any]
-            try expectEquals(1, notes.count)
+            try expectEquals(notesCount, notes.count)
         }
         else {
             try fail("No JSON in response")
@@ -85,10 +88,21 @@ final public class UserTests: APITest {
     }
 
     func logout() throws {
+        let note = makeNote()
         engine.auth = .none
         let response = try engine.post("/api/v1/notes", data: note)
         try expectEquals(401, response.status)
         try expectEquals("application/json; charset=utf-8", response.contentType)
+    }
+}
+
+// Private methods
+extension UserTests {
+    private func makeNote() -> [String: String] {
+        return [
+            "title": String.randomString(30),
+            "contents" : String.randomString(3000)
+        ]
     }
 }
 
