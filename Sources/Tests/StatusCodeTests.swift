8f012bda30a99efe75aac809a0dc72a457d6402e
Adrian Kosmaczewski <learn@akosma.training>
2017-10-13 20:54:34 +0200

    Added a test for not found routes

--- Sources/Tests/StatusCodeTests.swift
+++ Sources/Tests/StatusCodeTests.swift
@@ -2,10 +2,11 @@ final public class StatusCodeTests: APITest {
     override func scenario() -> [(String, APITest.TestMethod)]? {
         return [
             ("ping", ping),
+            ("notFound", notFound),
         ]
     }
 
-    public func ping() throws {
+    func ping() throws {
         let response = try engine.get("/ping")
         try expectEquals(response.status, 200, "When doing a ping, the returning message should contain a status of 200.")
         if let json = response.json {
@@ -14,6 +15,11 @@ final public class StatusCodeTests: APITest {
         else {
             try fail("No JSON in response")
         }
+    }
+
+    func notFound() throws {
+        let response = try engine.get("/whatever")
+        try expectEquals(404, response.status)
     }
 }
 
