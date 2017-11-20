ba3fccb4462ca7625e24a43985aadf8743a7c294
Adrian Kosmaczewski <learn@akosma.training>
2017-11-20 15:07:36 +0100

    Added API documentation

--- Sources/Scenarios/PublishingScenario.swift
+++ Sources/Scenarios/PublishingScenario.swift
@@ -1,7 +1,27 @@
 import Infrastructure
 
+/// Scenario with tests about the publishing feature of the API.
+/// Users are able to "publish" notes to the whole world. This is done
+/// through a "PUT" request that gives the note a "slug" that can be
+/// used to access the note in HTML format from any browser.
 final public class PublishingScenario: BaseScenario {
 
+    /// Runs several verifications about the publishing feature:
+    /// 1. Creates a random user.
+    /// 2. Logs in using the random credentials.
+    /// 3. Creates a note.
+    /// 4. Verifies that this new note is not published by default.
+    /// 5. Publishes the note.
+    /// 6. Checks that the logged in user can access publicly this note.
+    /// 7. Log out.
+    /// 8. Check again that the note is publicly available in HTML format.
+    /// 9. Log in again.
+    /// 10. Unpublish the note.
+    /// 11. Check that the note is not published anymore.
+    /// 12. Log out again.
+    /// 13. Check that the note is truly not published anymore.
+    ///
+    /// - Returns: A dictionary of string descriptions and test methods.
     public override func scenario() -> [(String, BaseScenario.TestMethod)]? {
         return [
             ("Create user", createUser),
@@ -14,10 +34,15 @@ final public class PublishingScenario: BaseScenario {
             ("Check published", checkPublished),
             ("Login", login),
             ("Unpublish note", unpublishNote),
-            ("Check not published", checkNotPublished),
+            ("Check note is not published again", checkNotPublished),
+            ("Logout", logout),
+            ("Check note is not published one last time", checkNotPublished),
         ]
     }
 
+    /// Verifies that a note is not published.
+    ///
+    /// - Throws: whatever is thrown.
     func checkNotPublished() throws {
         if let slug = slug {
             let response = try api.published(slug: slug)
@@ -28,31 +53,43 @@ final public class PublishingScenario: BaseScenario {
         }
     }
 
+    /// Publishes a note.
+    ///
+    /// - Throws: whatever is thrown.
     func publishNote() throws {
         if let id = noteUUID {
             let response = try api.publish(id: id)
             try expectStatusCode(.ok, response)
+            try expectSwiftAlpsVersionHeader(response)
         }
         else {
             try fail("No ID to retrieve note")
         }
     }
 
+    /// Verifies that a note is published.
+    ///
+    /// - Throws: whatever is thrown.
     func checkPublished() throws {
         if let slug = slug {
             let response = try api.published(slug: slug)
             try expectStatusCode(.ok, response)
             try expectContentType(.html, response)
+            try expectSwiftAlpsVersionHeader(response)
         }
         else {
             try fail("No ID to retrieve note")
         }
     }
 
+    /// Unpublishes a note.
+    ///
+    /// - Throws: whatever is thrown.
     func unpublishNote() throws {
         if let id = noteUUID {
             let response = try api.unpublish(id: id)
             try expectStatusCode(.ok, response)
+            try expectSwiftAlpsVersionHeader(response)
         }
         else {
             try fail("No ID to retrieve note")
