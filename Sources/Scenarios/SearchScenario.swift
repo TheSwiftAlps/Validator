a76b3b51815c440e63bf8a5c1e136eb254a6ad7f
Adrian Kosmaczewski <learn@akosma.training>
2017-11-20 15:40:22 +0100

    Added better searching of random words and API docs

--- Sources/Scenarios/SearchScenario.swift
+++ Sources/Scenarios/SearchScenario.swift
@@ -1,24 +1,44 @@
 import Infrastructure
+import RequestEngine
 
+/// Scenario testing the searching capabilities of the API
 public final class SearchScenario: BaseScenario {
 
+    /// This scenario does the following:
+    /// 1. Creates a random user.
+    /// 2. Logs in with this random user.
+    /// 3. Creates many random notes.
+    /// 4. Searches notes by contents, using a random word in one of the notes created previously.
+    /// 5. Searches notes by title, using a random word in one of the titles of the notes created previously.
+    ///
+    /// - Returns: A dictionary of string descriptions and test methods.
     public override func scenario() -> [(String, BaseScenario.TestMethod)]? {
         return [
             ("Create user", createUser),
             ("Login", login),
             ("Create many notes", createManyNotes),
-            ("Search", search),
+            ("Search by contents", searchByContents),
+            ("Search by title", searchByTitle),
         ]
     }
 
+    /// Creates many notes in the API.
+    ///
+    /// - Throws: whatever the underlying system throws.
     func createManyNotes() throws {
         for _ in 1...10 {
             try createNote()
         }
     }
 
-    func search() throws {
-        let response = try api.search(query: "Lorem")
+    /// Searches notes by a word in the contents.
+    ///
+    /// - Throws: whatever the underlying system throws.
+    func searchByContents() throws {
+        let words = noteContents?.split() ?? []
+        let randomWord = selectRandomWord(words: words)
+
+        let response = try api.search(query: randomWord)
         if let json = response.json {
             let notes = json["data"] as! [Any]
             try expectStatusCode(.ok, response)
@@ -27,7 +47,38 @@ public final class SearchScenario: BaseScenario {
             try expect(notes.count > 0)
         }
         else {
-            try fail("No JSON in response")
+            try fail("No note contained the word \"\(randomWord)\"")
         }
+    }
+
+    /// Searches notes by a word in the title.
+    ///
+    /// - Throws: whatever the underlying system throws.
+    func searchByTitle() throws {
+        let words = noteTitle?.split() ?? []
+        let randomWord = selectRandomWord(words: words)
+
+        let response = try api.search(query: randomWord)
+        if let json = response.json {
+            let notes = json["data"] as! [Any]
+            try expectStatusCode(.ok, response)
+            try expectContentType(.json, response)
+            try expectSwiftAlpsVersionHeader(response)
+            try expect(notes.count > 0)
+        }
+        else {
+            try fail("No note contained the word \"\(randomWord)\" in the title")
+        }
+    }
+    
+    /// Chooses a random word from an array of strings.
+    ///
+    /// - Parameter words: An array of strings to choose from.
+    /// - Returns: A random pick from the array.
+    private func selectRandomWord(words: [String]) -> String {
+        let count = words.count
+        let randomIndex = count.randomFromZeroToMe()
+        let randomWord = words[randomIndex]
+        return randomWord
     }
 }
