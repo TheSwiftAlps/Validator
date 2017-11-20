4b0ccb5090e0a6fca3b36a979348d15d926697b1
Adrian Kosmaczewski <learn@akosma.training>
2017-11-20 15:02:55 +0100

    Added API documentation

--- Sources/Infrastructure/ScenarioProgress.swift
+++ Sources/Infrastructure/ScenarioProgress.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+/// Data passed to the main program during the execution of a ScenarioSuite instance.
+///
+/// - success: Message indicating success.
+/// - info: Message indicating information.
+/// - error: Message indicating an error.
 public enum ScenarioProgress {
     case success(message: String)
     case info(message: String)
